a7e3572b1b0dc1a2c6f828c9cd07f700be9f5aab
Claudio Neto <claudionts@gmail.com>
2025-03-17 20:25:39 -0300

    change tmux.nix

--- programs/tmux.nix
+++ programs/tmux.nix
@@ -1,12 +1,33 @@
 { ... }: {
   programs.tmux = {
-    enable = false;
+    enable = true;
     extraConfig = ''
-      set-option -g focus-events on
-      set-option -a terminal-features 'rio:RGB'
+      unbind C-b
+      set -g prefix C-a
+      set -g mouse on
 
-      set -g default-command fish
-      set -g history-limit 4000
+      set-window-option -g mode-keys vi
+
+      bind C-c run "tmux save-buffer - | xclip -i -sel clip"
+      bind C-v run "tmux set-buffer $(xclip -o -sel clip); tmux paste-buffer"
+
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+
+      set -g @plugin 'erikw/tmux-powerline'
+
+
+      set-window-option -g mode-keys vi
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+      bind-key -r -T prefix       C-k              resize-pane -U 2 
+      bind-key -r -T prefix       C-j            resize-pane -D 2
+      bind-key -r -T prefix       C-h            resize-pane -L 2
+      bind-key -r -T prefix       C-l           resize-pane -R 2
     '';
   };
 }
