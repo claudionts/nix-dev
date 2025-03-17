6c4264e79db453508d71b307a28f09839ab49fac
Claudio Neto <claudionts@gmail.com>
2025-03-16 23:12:12 -0300

    fix #3

--- flake.nix
+++ flake.nix
@@ -1,29 +1,25 @@
 {
+  description = "Ambiente pessoal do Claudio com Nix Flakes e Home Manager";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      # Criação de uma derivação para o Home Manager
-      homeConfigurations.claudio = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs = { inherit inputs; };
-        modules = [ ./home-manager.nix ];
-      };
-
-      # Aqui criamos a derivação para o Home Manager, mas agora sem a tentativa de acessar 'activationPackage'
-      # O correto seria usar a configuração ativa diretamente
-      defaultPackage.x86_64-linux = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs = { inherit inputs; };
-        modules = [ ./home-manager.nix ];
-      };
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    homeConfigurations."claudio" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      extraSpecialArgs = { inherit nixpkgs; };
+      modules = [
+        ./home-manager.nix
+        {
+          home.username = "claudio";
+          home.homeDirectory = "/home/claudio";
+          home.stateVersion = "23.11";
+        }
+      ];
     };
+  };
 }
 
