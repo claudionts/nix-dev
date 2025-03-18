a896db90aa12ed2b8a1a3a3d2e222dc1d2d226be
Claudio Neto <claudionts@gmail.com>
2025-03-17 23:40:36 -0300

    add docker service

--- home.nix
+++ home.nix
@@ -1,4 +1,24 @@
-{...}: {
+{pkgs, ...}: {
+  programs.docker = {
+    enable = true;
+  };
+
+  # Adicionar Docker à lista de pacotes do sistema
+  environment.systemPackages = with pkgs; [
+    docker
+  ];
+
+  # (Opcional) Configurar o serviço systemd para Docker
+  systemd.services.docker = {
+    enable = true;
+    description = "Docker Daemon";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.docker}/bin/dockerd";
+      Restart = "always";
+      ExecReload = "${pkgs.docker}/bin/docker reload";
+    };
+  };
   home.file.".local/share/icons/teams.png".source = ./icons/teams.png;
 
   home.file.".local/share/applications/teams.desktop".text = ''
