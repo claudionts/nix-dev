a4807fe2be8d53075accc179f3fd6b6c335d22ff
Claudio Neto <claudionts@gmail.com>
2025-03-17 22:14:28 -0300

    disable neovim config

--- programs/neovim.nix
+++ programs/neovim.nix
@@ -1,32 +1,5 @@
 { pkgs, ... }: {
   programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    extraPackages = with pkgs; [
-      xclip
-      xsel
-      wl-clipboard
-    ];
-    viAlias = true;
-    vimAlias = true;
-
-    extraLuaConfig = ''
-      vim.opt.number = true
-
-      local o = vim.o
-      local g = vim.g
-
-      o.clipboard = "unnamedplus"
-
-      o.number = true
-      vim.opt.numberwidth = 1
-
-      o.swapfile = false
-
-      g.markdown_fenced_languages = {
-          "python", "elixir", "bash", "dockerfile", 'sh=bash'
-      }
-    "ignore config file"
-    '';
+    enable = false;
   };
 }
