46386d1dbe066f5cfa12d3f2ff7af9fec667940b
Claudio Neto <claudionts@gmail.com>
2025-09-15 18:10:50 -0300

    fix: correct neovim.nix syntax errors

---
+++ programs/neovim.nix
@@ -0,0 +1,138 @@
+{pkgs, ...}: {
+  programs.neovim = {
+    enable = true;
+    extraLuaConfig = ''
+               vim.opt.number = true
+
+                local o = vim.o
+             local g = vim.g
+
+             o.clipboard = "unnamedplus"
+
+             o.number = true
+
+             vim.opt.numberwidth = 1
+
+             o.swapfile = false
+
+                 g.markdown_fenced_languages = {
+                 "python", "elixir", "bash", "dockerfile", 'sh=bash'
+             }
+
+      vim.filetype.add({
+           extension = {
+             flow = 'json',
+           },
+         })
+    '';
+
+    plugins = with pkgs.vimPlugins; [
+      copilot-vim
+      {
+        plugin = nvim-treesitter.withAllGrammars;
+        type = "lua";
+        config = ''
+               require'nvim-treesitter.configs'.setup {
+              highlight = {enable = true}
+          }
+        '';
+      }
+      {
+        plugin = elixir-tools-nvim;
+        type = "lua";
+        config = ''
+          require("elixir").setup({
+            nextls = {enable = false},
+            elixirls = {enable = false},
+            projectionist = {enable = true},
+          })
+        '';
+      }
+      vim-projectionist
+
+      {
+        plugin = telescope-nvim;
+        type = "lua";
+        config = ''
+                     local telescope = require("telescope")
+                        telescope.setup({
+                            defaults = {
+                                vimgrep_arguments = {
+                                    "rg", "--color=never", "--no-heading", "--with-filename",
+                                    "--line-number", "--column", "--smart-case", "--hidden",
+                                    "--glob=!.git"
+                                }
+                            }
+                        })
+                        local builtin = require("telescope.builtin")
+                        vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
+                        vim.keymap.set('n', '<leader>fg', builtin.live_grep,
+                                       {desc = 'Telescope live grep'})
+          vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        '';
+      }
+      vimux
+      {
+        plugin = vim-test;
+        config = ''
+                 	  let g:test#echo_command = 0
+
+                 if exists('$TMUX')
+                   let g:test#preserve_screen = 1
+                   let g:test#strategy = 'vimux'
+          else
+            let g:test#strategy = 'neovim_sticky'
+                 endif
+
+                 nmap <silent> <leader>t :TestNearest<CR>
+                 nmap <silent> <leader>T :TestFile<CR>
+                 nmap <silent> <leader>a :TestSuite<CR>
+                 nmap <silent> <leader>l :TestLast<CR>
+                 nmap <silent> <leader>g :TestVisit<CR>
+        '';
+      }
+      vim-fugitive
+      {
+        plugin = gitlinker-nvim;
+        type = "lua";
+        config = "require'gitlinker'.setup()";
+      }
+      {
+        plugin = gitsigns-nvim;
+        type = "lua";
+        config = ''
+           require('gitsigns').setup{
+             on_attach = function(bufnr)
+            local gitsigns = require('gitsigns')
+
+            local function map(mode, l, r, opts)
+              opts = opts or {}
+              opts.buffer = bufnr
+              vim.keymap.set(mode, l, r, opts)
+            end
+
+            -- Navigation
+            map('n', ']c', function()
+              if vim.wo.diff then
+                vim.cmd.normal({']c', bang = true})
+              else
+                gitsigns.nav_hunk('next')
+              end
+            end)
+
+            map('n', '[c', function()
+              if vim.wo.diff then
+                vim.cmd.normal({'[c', bang = true})
+              else
+                gitsigns.nav_hunk('prev')
+              end
+            end)
+          end
+           }
+        '';
+      }
+
+      file-line
+    ];
+  };
+}
